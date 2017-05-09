7fa19eda69504cfe05f7ce900387756de52f5628
KletusVandamme <matt.hill.email@gmail.com>
2017-05-09 00:40:22 -0700

    works

--- client/js/finance.controller.js
+++ client/js/finance.controller.js
@@ -1,47 +1,9 @@
 "use strict";
 
 var app = angular.module('app',['ngRoute']);
-app.controller('FinanceTable', [
-		'$scope',
-		'$http',
-		'ngDialog',
-		// 'Utilities',
-		'$cookies',
-		financeController
-]);
+app.controller('FinanceTable', ['$scope', financeController]);
 
-function financeController(
-	$scope,
-	$http,
-	ngDialog,
-	// Utilities,
-	$cookies
-	) {
-
-  
-	// monkey patch .every function if it doesn't exist.
-	if (!Array.prototype.every)
-	{
-	   Array.prototype.every = function(fun /*, thisp*/)
-	   {
-	      var len = this.length;
-	      if (typeof fun != "function")
-	      throw new TypeError();
-	      
-	      var thisp = arguments[1];
-	      for (var i = 0; i < len; i++)
-	      {
-	         if (i in this && !fun.call(thisp, this[i], i, this))
-	         return false;
-	      }
-	      return true;
-	   };
-	}
+function financeController($scope) {
 
 }
 
-app.filter('html', function($sce) {
-  return function(val) {
-    return $sce.trustAsHtml(val);
-  };
-});
